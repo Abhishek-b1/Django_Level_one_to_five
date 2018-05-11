f68dc635ca2ccce7221e5f91d4281737f97a875e
abhishek banagude <abhishek.banagunde@kisanhub.com>
2018-05-11 16:41:41 +0530

    Abhishek Bangunde | Class based views | learning users | created simple template view with class based view

--- Class_based_view/adv_cbv/basic_app/views.py
+++ Class_based_view/adv_cbv/basic_app/views.py
@@ -2,12 +2,22 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import View   # View is the most generic view.
+from django.views.generic import View, TemplateView   # View is the most generic view.
 
 # Create your views here.
 
 
-class CBView(View):
-    def get(self, request):
-        return HttpResponse("Class based views are cool!")
+# class CBView(View):
+#     def get(self, request):
+#         return HttpResponse("Class based views are cool!")
+
+
+class IndexView(TemplateView):     # Inheriting from the TemplateView.
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTION!'
+        return context
+
 
