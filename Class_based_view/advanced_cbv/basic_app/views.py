25c6c835b22cba7075f0c6e3f2d66ae025f19cfd
abhishek banagude <abhishek.banagunde@kisanhub.com>
2018-05-14 19:08:09 +0530

    Abhishek Bangunde | Class based views | Can not resolve error "Reverse for 'update' not found. 'update' is not a valid view function or pattern name."

--- Class_based_view/advanced_cbv/basic_app/views.py
+++ Class_based_view/advanced_cbv/basic_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import (View, TemplateView, ListView, DetailView, CreateView, UpdateView)
 from . import models
 
 
@@ -10,14 +10,28 @@ class IndexView(TemplateView):
 class SchoolListView(ListView):
     context_object_name = 'schools'
     model = models.School
+    # Every details of list is saved because of ListView
+    # Here the ListView is creating a school list and naming it school_list. In the school_list school is coming from
+    # the .School, the ListView is lowering case of .School and renaming it as school_list which can be called in
+    # school_list.html in for loop or for other manipulations.
+    # Or if we want to change the default name then use context_object_name = "changed name" and this name can be
+    # called in the school_list.html for further manipulation.
 
 
-
-class SchoolDetailView(DetailView):
+class SchoolDetailView(DetailView):    # Detail view returns model in the lower case.
     context_object_name = 'schools_detail'
     model = models.School
     template_name = 'basic_app/school_detail.html'
 
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principle', 'location')    # Here creating the fields given in the parenthesis
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principle')    # Here we are updating name and principle of the school.
+    model = models.School
 
 
 
