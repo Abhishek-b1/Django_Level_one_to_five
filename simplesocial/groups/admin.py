ea19a65bf4372adbba6595787d48b6dd310fd81a
abhishek banagude <abhishek.banagunde@kisanhub.com>
2018-05-17 19:35:56 +0530

    Abhishek Bangunde | Social Blog Project | registered the views to urls.py file in groups app.

--- simplesocial/groups/admin.py
+++ simplesocial/groups/admin.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.contrib import admin
+from groups import models
 
 # Register your models here.
+# Here we will create a tabular inline: in the inline class we will be using basic classes to utilize the admin
+# interface to django website with the ability to edit models on the same page on parent model.
+
+
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+
+admin.site.register(models.Group)
+
+
