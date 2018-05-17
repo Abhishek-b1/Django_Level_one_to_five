f2cee6cad0664321b6578900021986d3dab6cb73
abhishek banagude <abhishek.banagunde@kisanhub.com>
2018-05-17 19:21:26 +0530

    Abhishek Bangunde | Social Blog Project | Created templates in groups app.

--- simplesocial/groups/views.py
+++ simplesocial/groups/views.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin)
+from django.core.urlresolvers import reverse
+from django.views import generic
+from groups.models import Group, GroupMember
 
 # Create your views here.
+
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+
+class SingleGroup(generic.DeleteView):
+    model = Group
+
+
+class ListGroups(generic.ListView):
+    model = Group
