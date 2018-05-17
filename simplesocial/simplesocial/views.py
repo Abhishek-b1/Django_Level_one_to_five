ae3c89d901fb3cb406f9946dd8483455bb316871
abhishek banagude <abhishek.banagunde@kisanhub.com>
2018-05-17 16:15:21 +0530

    Abhishek Bangunde | Social Blog Project | Created two more apps posts and groups and their corresponding templates.

--- simplesocial/simplesocial/views.py
+++ simplesocial/simplesocial/views.py
@@ -2,6 +2,10 @@
 from django.views.generic import TemplateView
 
 
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+
 class HomePage(TemplateView):
     template_name = 'index.html'
 
